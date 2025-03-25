6fa0a9f59e09946dc1c0463bce9dd1b4a92d6021
Emmanuel-Diko <emmanuelnnachi@punch.agency>
2025-03-25 06:17:13 -0700

    made changes to authService

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -6,43 +6,39 @@ const UserContext = createContext();
 // Custom hook to use the user context
 export const useUser = () => useContext(UserContext);
 
-
 // Provider component to wrap the application
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    // Load user from localStorage (useful for page refreshes)
-    useEffect(() => {
-        const storedUser = localStorage.getItem("user");
-        if (storedUser) {
-            setUser(JSON.parse(storedUser));
-        }
-    }, []);
+  // Load user from localStorage (useful for page refreshes)
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser && storedUser !== "undefined") {
+       setUser(JSON.parse(storedUser));
+    }
+  }, [user]);
 
+  // Function to log in the user and store data
+  const loginUser = (userData) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
 
-    // Function to log in the user and store data
-    const loginUser = (userData) => {
-        setUser(userData);
-        localStorage.setItem("user", JSON.stringify(userData));
-    };
+  // Function to handle user signup (same logic as login)
+  const signupUser = (userData) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
 
-     // Function to handle user signup (same logic as login)
-     const signupUser = (userData) => {
-        setUser(userData);
-        localStorage.setItem("user", JSON.stringify(userData));
-    };
+  // Function to log out the user
+  const logoutUser = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
 
-    // Function to log out the user
-    const logoutUser = () => {
-        setUser(null);
-        localStorage.removeItem("user");
-    };
-
-
-    return (
-        <UserContext.Provider value={{ user, loginUser, logoutUser, signupUser  }}>
-            {children}
-        </UserContext.Provider>
-    );
-
+  return (
+    <UserContext.Provider value={{ user, loginUser, logoutUser, signupUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
