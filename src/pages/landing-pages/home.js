fd3b6ad794ff4194ceb3e30900850ee0450c654a
IgomigoFataiVictor <igomigofatai@punch.agency>
2025-08-27 10:37:11 +0100

    feat: Enhance contact form functionality and user feedback
    - Implemented state management for form values, submission status, and error/success messages in the contact form.
    - Added validation for required fields and email format to improve user input handling.
    - Integrated Snackbar and Alert components for displaying success and error messages upon form submission.
    - Refactored the form layout to ensure a better user experience during submission.

--- src/pages/landing-pages/home.js
+++ src/pages/landing-pages/home.js
@@ -1,49 +1,52 @@
-import React, { useEffect } from 'react'
-import { Box, Typography } from '@mui/material'
-import banner from '../../assets/images/YCC-home-banner-new.png'
-import Section1Home from '../../components/home/section1-home'
-import Section2Home from '../../components/home/section2-home'
-import Section3Home from '../../components/home/section3-home'
-import LandingPageFooter from '../../components/landing-page-footer'
-import Section4Home from '../../components/home/section4-home'
-import LandingPageBanner from '../../components/landing-page-banner'
-import Chatbot from '../../components/chatbot/chatbot'
-
+import React, { useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import banner from "../../assets/images/YCC-home-banner-new.png";
+import Section1Home from "../../components/home/section1-home";
+import Section2Home from "../../components/home/section2-home";
+import Section3Home from "../../components/home/section3-home";
+import LandingPageFooter from "../../components/landing-page-footer";
+import Section4Home from "../../components/home/section4-home";
+import LandingPageBanner from "../../components/landing-page-banner";
+import Chatbot from "../../components/chatbot/chatbot";
 
 const HomeLandingPage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  const backgroundImage = banner
+  const backgroundImage = banner;
   const header = (
     <>
       Your Destination for Crew <GradientText>resources</GradientText> &{" "}
       <GradientText>Vessel</GradientText> Management
     </>
   );
-  const subtext1 = 'Find local service providers and order your departments supplies. Through our AI supported Global Network. No matter the ask or the time Yacht Crew Center is here to help you succeed.'
-  const subtext2 = 'Welcome to Yacht Crew Center, LLC —the trusted hub for Yachting professionals worldwide. Founded by yacht crew for yacht crew, our platform combines industry expertise, personalized support, and global reach to empower your career and simplify vessel management. Whatever your position, Yacht Crew Center is here to help you succeed on and off the water.'
+  const subtext1 =
+    "Find local service providers and order your departments supplies. Through our AI supported Global Network. No matter the ask or the time Yacht Crew Center is here to help you succeed.";
+  const subtext2 =
+    "Welcome to Yacht Crew Center, LLC —the trusted hub for Yachting professionals worldwide. Founded by yacht crew for yacht crew, our platform combines industry expertise, personalized support, and global reach to empower your career and simplify vessel management. Whatever your position, Yacht Crew Center is here to help you succeed on and off the water.";
   const button1 = {
-    text: 'About Yacht Crew Center',
-    path: '/about-us'
-  }
+    text: "About Yacht Crew Center",
+    path: "/about-us",
+  };
   const button2 = {
-    text: 'Onboard Vendors & Services',
-    path: '/vendor-services'
-  }
-  const page = 'home'
+    text: "Onboard Vendors & Services",
+    path: "/vendor-services",
+  };
+  const page = "home";
   return (
-    <Box sx={{
-      display: "flex",
-      flexDirection: "column",
-      gap: {
-        xs: "47px",  // Smaller gap on mobile
-        sm: "60px",  // Medium gap on small screens
-        md: "90px",  // Larger gap on tablets
-        lg: "120px", // Default gap for large screens
-      },    
-      maxWidth: '100vw'
-    }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: {
+          xs: "47px", // Smaller gap on mobile
+          sm: "60px", // Medium gap on small screens
+          md: "90px", // Larger gap on tablets
+          lg: "120px", // Default gap for large screens
+        },
+        maxWidth: "100vw",
+      }}
+    >
       <LandingPageBanner
         backgroundImage={backgroundImage}
         header={header}
@@ -63,7 +66,7 @@ const HomeLandingPage = () => {
   );
 };
 
-const linearGradientLight =  `linear-gradient(86.8deg, #FFFFFF -51.91%, #209DEB 84.68%)`;
+const linearGradientLight = `linear-gradient(86.8deg, #FFFFFF -51.91%, #209DEB 84.68%)`;
 const GradientText = (props) => (
   <Typography
     component="span"
@@ -74,10 +77,10 @@ const GradientText = (props) => (
       fontWeight: 600,
       fontFamily: "Plus Jakarta Sans, sans-serif",
       fontSize: {
-        xs: "30px",  // Smaller font for mobile
-        sm: "35px",  // Slightly bigger for small tablets
-        md: "40px",  // Medium screens
-        lg: "50px",  // Default for large screens
+        xs: "30px", // Smaller font for mobile
+        sm: "35px", // Slightly bigger for small tablets
+        md: "40px", // Medium screens
+        lg: "50px", // Default for large screens
       },
       lineHeight: {
         xs: "38px", // Smaller line height for mobile
@@ -94,4 +97,4 @@ const GradientText = (props) => (
   </Typography>
 );
 
-export default HomeLandingPage
+export default HomeLandingPage;
